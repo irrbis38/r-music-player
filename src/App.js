d8f5ffbe7dcf0b408c74a0117b7b5cbfc5269a42
Vasil Ilyuhin <v.ilyuhin@gmail.com>
2023-04-02 08:50:15 +0300

    feat: add data from data.js, add styles to Song component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { useState } from "react";
+
 import { Player } from "./components/Player/Player";
 import { Song } from "./components/Song/Song";
 
+import chillHop from "./data";
+
 import "./app.scss";
 
-export const App = () => (
-  <div className="App">
-    <Song />
-    <Player />
-  </div>
-);
+export const App = () => {
+  const [songs, setSong] = useState(() => chillHop());
+  const [currentSong, setCurrentSong] = useState(songs[2]);
+  return (
+    <div className="App">
+      <Song currentSong={currentSong} />
+      <Player />
+    </div>
+  );
+};
--- src/components/Song/Song.jsx
+++ src/components/Song/Song.jsx
@@ -1,9 +1,13 @@
 import s from "./Song.module.scss";
 
-export const Song = () => (
-  <div className={s.song}>
-    <div className="img">Picture</div>
-    <p>Song</p>
-    <p>Artist</p>
-  </div>
-);
+export const Song = ({ currentSong }) => {
+  return (
+    <div className={s.song}>
+      <div className={s.song__cover}>
+        <img src={currentSong.cover} alt={currentSong.name} />
+      </div>
+      <p className={s.song__name}>{currentSong.name}</p>
+      <p className={s.song__artist}>{currentSong.artist}</p>
+    </div>
+  );
+};
